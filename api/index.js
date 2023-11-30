f8cdf6ef62cb3e0e030db98cdd815d6cc9cae742
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-11-30 19:53:53 +0400

    server base code set

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 // back end file
 // npm i express
 // node api/index.js to start server
+// nodemon install
 import express from 'express'
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000!');
 });

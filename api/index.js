e6087fff56e7dceae792904d1e8791e8b3b6e295
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-11-30 19:36:44 +0400

    Update index.js

---
+++ api/index.js
@@ -0,0 +1,11 @@
+// back end file
+// npm i express
+
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});

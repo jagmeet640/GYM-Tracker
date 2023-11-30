3f40b88ddf4b73af88a2380ae6eec1965e39a012
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-11-30 14:06:45 +0400

    audited code and created js for different pages

--- gym_tracker/src/App.js
+++ gym_tracker/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <h1 className="text-red-800">
+        Hello world!
+      </h1>
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- gym_tracker/tailwind.config.js
+++ gym_tracker/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-

892f23b48fe2cc08173e099dbcf016fdb79d52ee
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-12-02 22:08:03 +0400

    installed bcryptjs for encryption
    npm i bcryptjs

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,11 +1,18 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+// npm i bcryptjs ~ this is for encrypting the passwords to make it more secure and keep the useres data safe
 
 
 export const signup = async (req, res) => {
     // srtoring data from the get req as a const object 
     const {username, email, password} = req.body;
     // now storing this data in the mongodb database
-    const newUser = new User({username, email, password});
+
+    // need to hash/encryp the password before storing
+    const hashPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({username, email, hashPassword});
     // saving the user info
     // when using await means that wait till the data has been saved we need to change the function to a async function
     await newUser.save();

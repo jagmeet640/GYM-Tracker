ab076ed48709b63ad76417b5029481693315337d
jagmeet640 <jagmeetghotra2002@gmail.com>
2024-01-09 02:11:31 +0400

    added rep counter and functionality
    Functionallity to increase counter instances and to remove

--- gt/src/pages/Home.js
+++ gt/src/pages/Home.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Home() {
+  const [counters, setCounters] = useState([{ id: 1, value: 0 }]);
+
+  const increaseReps = (id) => {
+    setCounters((prevCounters) =>
+      prevCounters.map((counter) =>
+        counter.id === id ? { ...counter, value: counter.value + 1 } : counter
+      )
+    );
+  };
+
+  const resetReps = (id) => {
+    setCounters((prevCounters) =>
+      prevCounters.map((counter) =>
+        counter.id === id ? { ...counter, value: 0 } : counter
+      )
+    );
+  };
+
+  const addCounter = () => {
+    const newCounterId = counters.length + 1;
+    setCounters((prevCounters) => [...prevCounters, { id: newCounterId, value: 0 }]);
+  };
+
+  const removeCounter = (id) => {
+    setCounters((prevCounters) => prevCounters.filter((counter) => counter.id !== id));
+  }
+
   return (
-    <div>Home</div>
-  )
+    <div className='p-8'>
+      {counters.map((counter) => (
+        <div key={counter.id} className='bg-white p-6 rounded-lg shadow-md mb-4'>
+          <div className='text-xl font-bold mb-4'>Excersise #{counter.id} Reps : {counter.value}</div>
+          <button
+            onClick={() => increaseReps(counter.id)}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2'
+          >
+            Increase reps
+          </button>
+          <button
+            onClick={() => resetReps(counter.id)}
+            className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2'
+          >
+            Reset
+          </button>
+
+          <button
+            onClick={() => removeCounter(counter.id)}
+            className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+          >
+            Remove Counter
+          </button>
+          
+
+        </div>
+      ))}
+      <button
+        onClick={addCounter}
+        className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2'
+      >
+        Add Another Counter
+      </button>
+
+      
+    </div>
+  );
 }
 
-
 export default Home;
+

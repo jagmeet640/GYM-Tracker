ade7cebf08640d131d965ab212e31c01b55a4349
jagmeet640 <jagmeetghotra2002@gmail.com>
2024-01-15 18:31:27 +0400

    Update RepExcCombTest.js

--- gt/src/pages/RepExcCombTest.js
+++ gt/src/pages/RepExcCombTest.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function RepExcCombTest() {
-  const [counters, setCounters] = useState([{ id: 1, value: 0 }]);
+  const [counters, setCounters] = useState([{ id: 1, value: 0, name: '' }]);
   const uniqueId = uuidv4();
 
   const increaseReps = (id) => {
@@ -23,7 +23,6 @@ function RepExcCombTest() {
 
   const addCounter = () => {
 
-    // const newCounterId = counters.length + 1;
     const newCounterId = uniqueId;
     setCounters((prevCounters) => [...prevCounters, { id: newCounterId, value: 0 }]);
   };
@@ -32,47 +31,30 @@ function RepExcCombTest() {
     setCounters((prevCounters) => prevCounters.filter((counter) => counter.id !== id));
   }
 
-  const uniqueExcersiseId = uuidv4();
-  const [excersises, setExcersise] = useState([{ id: uniqueExcersiseId, excersiseName: '' }]);
+  const handleExcersiseName = (e, id) => {
+    setCounters((prevCounters) => 
+      prevCounters.map((counter) => 
+        counter.id === id ? {...counter, name: e.target.value} : counter
+      )
+    )
+    
+  }
 
-  const handleExcersiseNameChange = (e, id) => {
-    setExcersise((prevExcersise) =>
-      prevExcersise.map((excersise) =>
-        excersise.id === id ? { ...excersise, excersiseName: e.target.value } : excersise
-      ));
+  // todo
+  // add another use state for storing the name of the excersise entered and have it associated with the rep couter id
 
-  };
-  const addExcersiseLog = () => {
-    const newExcersiseId = uniqueExcersiseId;
-    setExcersise((prevExcersise) => [...prevExcersise, { id: newExcersiseId, value: '' }])
-  };
-
-  const removeExcersise = (id) => {
-    setExcersise((prevExcersise) => prevExcersise.filter((excersise) => excersise.id !== id));
-  };
 
   return (
     <div className='p-8'>
       {counters.map((counter) => (
         <div key={counter.id} className='bg-white p-6 rounded-lg shadow-md mb-4'>
 
-          {excersises.map((excersise) => (
-            <div key={excersise.id}>
-              <div>
-                <input type="text" name="Excersise" value={excersise.excersiseName} onChange={() => handleExcersiseNameChange(excersise.id)} />
-              </div>
-
-              <button
-                onClick={() => removeExcersise(excersise.id)}
-                className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-2 mt-2'
-              >Remove Excersise</button>
-            </div>
-          ))}
-
-          <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2' onClick={addExcersiseLog}> Add another excersise</button>
-
-
-          <div className='text-xl font-bold mb-4'>Excersise #{counter.id} Reps : {counter.value}</div>
+          <div className='text-xl font-bold mb-4'>Enter Excersise Below</div>
+          <div className='text-xl font-bold mb-4'>
+                <input type="text" name="Excersise" value={counter.name} onChange={() => handleExcersiseName(counter.id)}/>
+          </div>
+        
+          <div className='text-xxl font-bold mb-4'>Reps : {counter.value}</div>
           <button
             onClick={() => increaseReps(counter.id)}
             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2'
